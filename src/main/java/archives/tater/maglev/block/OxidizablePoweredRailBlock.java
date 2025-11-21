Entropy159/Maglev

Port to Polymer
Fully functional polymer port, the one thing is the rails look like regular rails, no way to distinguish them currently.

src/main/java/archives/tater/maglev/block/OxidizablePoweredRailBlock.java
@@ -1,17 +1,20 @@
 package archives.tater.maglev.block;
 
 import archives.tater.maglev.HasOxidationLevel;
+import eu.pb4.polymer.core.api.block.PolymerBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Oxidizable;
 import net.minecraft.block.PoweredRailBlock;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import xyz.nucleoid.packettweaker.PacketContext;
 
 import static archives.tater.maglev.init.MaglevDataAttachments.SPEED_MULTIPLIER;
 
-public class OxidizablePoweredRailBlock extends PoweredRailBlock implements Oxidizable, HasOxidationLevel, VariantPoweredRail {
+public class OxidizablePoweredRailBlock extends PoweredRailBlock implements Oxidizable, HasOxidationLevel, VariantPoweredRail, PolymerBlock {
     private final OxidationLevel oxidationLevel;
 
     public OxidizablePoweredRailBlock(OxidationLevel oxidationLevel, Settings settings) {
@@ -49,5 +52,10 @@ public class OxidizablePoweredRailBlock extends PoweredRailBlock implements Oxid
             minecart.setAttached(SPEED_MULTIPLIER, OxidizablePoweredRailBlock.getSpeedMultiplier(oxidizable.getDegradationLevel()));
         else
             minecart.removeAttached(SPEED_MULTIPLIER);
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState blockState, PacketContext packetContext) {
+        return Blocks.POWERED_RAIL.getStateWithProperties(blockState);
     }
 }

src/main/java/archives/tater/maglev/block/OxidizableRailBlock.java
@@ -1,14 +1,17 @@
 package archives.tater.maglev.block;
 
 import archives.tater.maglev.HasOxidationLevel;
+import eu.pb4.polymer.core.api.block.PolymerBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Oxidizable;
 import net.minecraft.block.RailBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import xyz.nucleoid.packettweaker.PacketContext;
 
-public class OxidizableRailBlock extends RailBlock implements Oxidizable, HasOxidationLevel {
+public class OxidizableRailBlock extends RailBlock implements Oxidizable, HasOxidationLevel, PolymerBlock {
     private final OxidationLevel oxidationLevel;
 
     public OxidizableRailBlock(OxidationLevel oxidationLevel, Settings settings) {
@@ -29,5 +32,10 @@ public class OxidizableRailBlock extends RailBlock implements Oxidizable, HasOxi
     @Override
     public OxidationLevel getDegradationLevel() {
         return oxidationLevel;
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState blockState, PacketContext packetContext) {
+        return Blocks.RAIL.getStateWithProperties(blockState);
     }
 }

Entropy159/MaglevPolymer

src/main/java/archives/tater/maglev/block/OxidizablePoweredRailBlock.java
@@ -1,6 +1,7 @@
 package archives.tater.maglev.block;
 
 import archives.tater.maglev.HasOxidationLevel;
+import eu.pb4.polymer.core.api.block.PolymerBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
@@ -11,7 +12,7 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import static archives.tater.maglev.init.MaglevDataAttachments.SPEED_MULTIPLIER;
 
-public class OxidizablePoweredRailBlock extends PoweredRailBlock implements WeatheringCopper, HasOxidationLevel, VariantPoweredRail {
+public class OxidizablePoweredRailBlock extends PoweredRailBlock implements WeatheringCopper, HasOxidationLevel, VariantPoweredRail, PolymerBlock {
     private final WeatherState oxidationLevel;
 
     public OxidizablePoweredRailBlock(WeatherState oxidationLevel, Properties settings) {
@@ -49,5 +50,10 @@ public class OxidizablePoweredRailBlock extends PoweredRailBlock implements Weat
             minecart.setAttached(SPEED_MULTIPLIER, OxidizablePoweredRailBlock.getSpeedMultiplier(oxidizable.getAge()));
         else
             minecart.removeAttached(SPEED_MULTIPLIER);
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState blockState, PacketContext packetContext) {
+        return Blocks.POWERED_RAIL.getStateWithProperties(blockState);
     }
 }

src/main/java/archives/tater/maglev/block/OxidizableRailBlock.java
@@ -1,6 +1,7 @@
 package archives.tater.maglev.block;
 
 import archives.tater.maglev.HasOxidationLevel;
+import eu.pb4.polymer.core.api.block.PolymerBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
@@ -8,7 +9,7 @@ import net.minecraft.world.level.block.RailBlock;
 import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class OxidizableRailBlock extends RailBlock implements WeatheringCopper, HasOxidationLevel {
+public class OxidizableRailBlock extends RailBlock implements WeatheringCopper, HasOxidationLevel, PolymerBlock {
     private final WeatherState oxidationLevel;
 
     public OxidizableRailBlock(WeatherState oxidationLevel, Properties settings) {
@@ -29,5 +30,10 @@ public class OxidizableRailBlock extends RailBlock implements WeatheringCopper, 
     @Override
     public WeatherState getAge() {
         return oxidationLevel;
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState blockState, PacketContext packetContext) {
+        return Blocks.RAIL.getStateWithProperties(blockState);
     }
 }